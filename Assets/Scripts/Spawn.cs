e88a265c85cc03f4db11df4c7f5e89aad32ad27d
JohnyDux <126690778+JohnyDux@users.noreply.github.com>
2023-10-24 18:42:24 +0100

    A Patient goes to waiting area

--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -2,30 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spawn : MonoBehaviour
-{
+public class Spawn : MonoBehaviour {
+
     public GameObject patientPrefab;
     public int numPatients;
-    
-    void Start()
-    {
-        for(int i = 0; i < numPatients; i++)
-        {
+
+    void Start() {
+
+        for (int i = 0; i < numPatients; ++i) {
+
             Instantiate(patientPrefab, this.transform.position, Quaternion.identity);
         }
 
-        Invoke("SpawnPatient", 5);
+        Invoke("SpawnPatient", 5.0f);
     }
 
-    void SpawnPatient()
-    {
+    void SpawnPatient() {
+
         Instantiate(patientPrefab, this.transform.position, Quaternion.identity);
-        Invoke("SpawnPatient", Random.Range(2,10));
+        Invoke("SpawnPatient", Random.Range(2.0f, 10.0f));
     }
 
-    
-    void Update()
-    {
-        
+    void Update() {
+
     }
 }
