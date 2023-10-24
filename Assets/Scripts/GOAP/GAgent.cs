9ce01cc3b84bcb97bae4d32bff2f87d59bb00ff5
JohnyDux <126690778+JohnyDux@users.noreply.github.com>
2023-10-24 18:50:38 +0100

    Multistep plan working

--- Assets/Scripts/GOAP/GAgent.cs
+++ Assets/Scripts/GOAP/GAgent.cs
@@ -48,8 +48,8 @@ public class GAgent : MonoBehaviour {
     void LateUpdate() {
 
         if (currentAction != null && currentAction.running) {
-
-            if (currentAction.agent.hasPath && currentAction.agent.remainingDistance < 1.0f) {
+            float distanceToTarget = Vector3.Distance(currentAction.target.transform.position, this.transform.position);
+            if (currentAction.agent.hasPath && distanceToTarget < 2f) {
 
                 if (!invoked) {
 
