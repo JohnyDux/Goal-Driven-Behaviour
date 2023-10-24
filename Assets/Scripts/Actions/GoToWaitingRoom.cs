a2671123a35b97b995843aa30a2c1b7445a2215b
JohnyDux <126690778+JohnyDux@users.noreply.github.com>
2023-10-24 19:26:37 +0100

    Matching Agents with Agents

--- Assets/Scripts/Actions/GoToWaitingRoom.cs
+++ Assets/Scripts/Actions/GoToWaitingRoom.cs
@@ -8,6 +8,7 @@ public class GoToWaitingRoom : GAction {
     public override bool PostPerform() {
 
         GWorld.Instance.GetWorld().ModifyState("Waiting", 1);
+        GWorld.Instance.AddPatient(this.gameObject);
         return true;
     }
 }
